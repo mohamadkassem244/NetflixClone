df0535bb5c3bab19e197a2786db62f9adb53857b
mohamadkassem <mohamadkassem244@gmail.com>
2024-04-01 18:56:30 +0300

    fix mobile scroll v2

--- script.js
+++ script.js
@@ -4,45 +4,60 @@ function enableHorizontalScrolling(containerId) {
   var startX;
   var scrollLeft;
 
-  container.addEventListener("mousedown", function (e) {
+  // Function to handle mouse down event
+  function handleMouseDown(e) {
     isDragging = true;
     startX = e.pageX - container.offsetLeft;
     scrollLeft = container.scrollLeft;
-  });
+  }
 
-  container.addEventListener("mouseleave", function () {
-    isDragging = false;
-  });
-
-  container.addEventListener("mouseup", function () {
-    isDragging = false;
-  });
-
-  container.addEventListener("mousemove", function (e) {
+  // Function to handle mouse move event
+  function handleMouseMove(e) {
     if (!isDragging) return;
     e.preventDefault();
     var x = e.pageX - container.offsetLeft;
     var walk = (x - startX) * 1;
     container.scrollLeft = scrollLeft - walk;
-  });
+  }
 
-  container.addEventListener("touchstart", function (e) {
+  // Function to handle mouse up event
+  function handleMouseUp() {
+    isDragging = false;
+  }
+
+  // Function to handle touch start event
+  function handleTouchStart(e) {
+    if (e.touches.length > 1) {
+      return; // Ignore multitouch gestures
+    }
     isDragging = true;
     startX = e.touches[0].pageX - container.offsetLeft;
     scrollLeft = container.scrollLeft;
-  });
+  }
 
-  container.addEventListener("touchmove", function (e) {
-    if (!isDragging) return;
+  // Function to handle touch move event
+  function handleTouchMove(e) {
+    if (!isDragging || e.touches.length > 1) return;
     e.preventDefault();
     var x = e.touches[0].pageX - container.offsetLeft;
     var walk = (x - startX) * 1;
     container.scrollLeft = scrollLeft - walk;
-  });
+  }
 
-  container.addEventListener("touchend", function () {
+  // Function to handle touch end event
+  function handleTouchEnd() {
     isDragging = false;
-  });
+  }
+
+  // Add event listeners for mouse events
+  container.addEventListener("mousedown", handleMouseDown);
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseup", handleMouseUp);
+
+  // Add event listeners for touch events
+  container.addEventListener("touchstart", handleTouchStart);
+  container.addEventListener("touchmove", handleTouchMove);
+  container.addEventListener("touchend", handleTouchEnd);
 }
 
 enableHorizontalScrolling("scrollContainer1");
