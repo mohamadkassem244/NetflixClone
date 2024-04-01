b0c085dbc95204ebfbdef8236437f113e25ca718
mohamadkassem <mohamadkassem244@gmail.com>
2024-04-01 18:41:35 +0300

    fix mobile scroll

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
 function enableHorizontalScrolling(containerId) {
-  
-    var container = document.getElementById(containerId);
-    var isMouseDown = false;
-    var startX;
-    var scrollLeft;
-  
-    function handleMouseDown(e) {
-      isMouseDown = true;
-      startX = e.pageX - container.offsetLeft;
-      scrollLeft = container.scrollLeft;
-    }
-  
-    function handleMouseLeave() {
-      isMouseDown = false;
-    }
-  
-    function handleMouseUp() {
-      isMouseDown = false;
-    }
-  
-    function handleMouseMove(e) {
-      if (!isMouseDown) return;
-      e.preventDefault();
-      var x = e.pageX - container.offsetLeft;
-      var walk = (x - startX) * 1;
-      container.scrollLeft = scrollLeft - walk;
-    }
-  
-    function handleTouchStart(e) {
-      isMouseDown = true;
-      startX = e.touches[0].pageX - container.offsetLeft;
-      scrollLeft = container.scrollLeft;
-    }
-  
-    function handleTouchMove(e) {
-      if (!isMouseDown) return;
-      e.preventDefault();
-      var x = e.touches[0].pageX - container.offsetLeft;
-      var walk = (x - startX) * 1;
-      container.scrollLeft = scrollLeft - walk;
-    }
-    container.addEventListener("mousedown", handleMouseDown);
-    container.addEventListener("mouseleave", handleMouseLeave);
-    container.addEventListener("mouseup", handleMouseUp);
-    container.addEventListener("mousemove", handleMouseMove);
-  
-    container.addEventListener("touchstart", handleTouchStart);
-    container.addEventListener("touchmove", handleTouchMove);
-  }
+  var container = document.getElementById(containerId);
+  var isDragging = false;
+  var startX;
+  var scrollLeft;
 
+  container.addEventListener("mousedown", function (e) {
+    isDragging = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("mouseleave", function () {
+    isDragging = false;
+  });
+
+  container.addEventListener("mouseup", function () {
+    isDragging = false;
+  });
+
+  container.addEventListener("mousemove", function (e) {
+    if (!isDragging) return;
+    e.preventDefault();
+    var x = e.pageX - container.offsetLeft;
+    var walk = (x - startX) * 1;
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  container.addEventListener("touchstart", function (e) {
+    isDragging = true;
+    startX = e.touches[0].pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("touchmove", function (e) {
+    if (!isDragging) return;
+    e.preventDefault();
+    var x = e.touches[0].pageX - container.offsetLeft;
+    var walk = (x - startX) * 1;
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  container.addEventListener("touchend", function () {
+    isDragging = false;
+  });
+}
 
 enableHorizontalScrolling("scrollContainer1");
 enableHorizontalScrolling("scrollContainer2");
